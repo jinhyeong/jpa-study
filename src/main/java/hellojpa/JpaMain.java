1f245a1e107ba5f0ac0212be722dbf1b9c8a5dbd
jinhyeong <jinhyeong.b@gmail.com>
2022-02-03 23:12:20 +0900

    Select member

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 	public static void main(String[] args) {
@@ -12,12 +13,13 @@ public class JpaMain {
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
 
-		Member member = new Member();
-		member.setId(2L);
-		member.setName("HelloB");
+		//		Member member = em.find(Member.class, 1L);
 
+		List<Member> result = em.createQuery("select m from Member as m", Member.class)
+				.setFirstResult(5)
+				.setMaxResults(8)
+				.getResultList();
 
-		em.persist(member);
 		transaction.commit();
 
 		em.close();
